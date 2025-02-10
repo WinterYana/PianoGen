567775890545a0a7a7c72067585dd677e15e615a
Winter_Yana <winteryana@yandex.ru>
2025-02-10 22:44:20 +0300

    Добавлены функции extract_notes_from_segment (разбиение отрезка на временные шаги с извлечением информации о нотах) и prepare_lstm_data (нормализация последовательности для LSTM)

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,12 +2,12 @@ from music21 import *
 import copy
 from tqdm import tqdm
 import os
-import math
+import numpy as np
 
 
 def process_midi(midi_data, segment_size=8, bpm=120, time_signature="4/4", measure_length=4):
     parts = midi_data.parts
-    all_notes = [list(part.flat.notesAndRests) for part in parts]
+    all_notes = [list(part.flatten().notesAndRests) for part in parts]
     segment_length = segment_size * measure_length
 
     segments = []
@@ -72,18 +72,85 @@ def process_midi(midi_data, segment_size=8, bpm=120, time_signature="4/4", measu
     return segments
 
 
+def extract_notes_from_segment(segment, time_step=1/12, segment_length=32, max_notes=6):
+    num_time_steps = int(segment_length / time_step)
+    data = [[] for _ in range(num_time_steps)]
+
+    for i, part in enumerate(segment.parts):
+        hand = 1 if i == 0 else 0
+        for elem in part.flatten().notesAndRests:
+            start_step = round(elem.offset / time_step)
+            end_step = min(start_step + round(elem.quarterLength / time_step), num_time_steps)
+
+            if isinstance(elem, note.Note):
+                note_info = [elem.pitch.midi, elem.offset, int(elem.volume.velocity) if elem.volume.velocity else 64, hand]
+                for step in range(start_step, min(end_step, num_time_steps)):
+                    data[step].append(note_info)
+            elif isinstance(elem, chord.Chord):
+                for pitch in elem.pitches:
+                    note_info = [pitch.midi, elem.offset, int(elem.volume.velocity) if elem.volume.velocity else 64, hand]
+                    for step in range(start_step, min(end_step, num_time_steps)):
+                        data[step].append(note_info)
+            elif isinstance(elem, note.Rest):  # Пауза
+                note_info = [-1, elem.offset, 0, hand]
+                for step in range(start_step, min(end_step, num_time_steps)):
+                    data[step].append(note_info)
+
+    for step in range(num_time_steps):
+        data[step] = data[step][:max_notes]
+        while len(data[step]) < max_notes:
+            data[step].append([-1, step * time_step, 0, 0])
+    #convert_to_midi(data)
+    return data
+
+
+def prepare_lstm_data(data, sequence_length=64, duration_steps=384, segment_length=32):
+    sequences = []
+    next_steps = []
+
+    segment_data = np.array(data, dtype=np.float32)
+    for i in range(len(segment_data) - sequence_length):
+        sequences.append(segment_data[i:i + sequence_length])
+        next_steps.append(segment_data[i + sequence_length])
+
+    X = np.array(sequences)
+    y = np.array(next_steps)
+
+    X[..., 0] /= 127  # Нормализация MIDI-ноты (0-127 → 0-1)
+    X[..., 1] /= segment_length  # Нормализация offset (0 - 32 → 0 - 1)
+    X[..., 2] /= duration_steps  # Нормализация длительности (1-384 → 0-1)
+    X[..., 3] /= 127  # Нормализация velocity (0-127 → 0-1)
+
+    y[..., 0] /= 127
+    y[..., 1] /= segment_length
+    y[..., 2] /= duration_steps
+    y[..., 3] /= 127
+    return X, y
+
+
+count = 0
 files_path = '../data/midi'
 output_path = '../data/processed'
 environment.set('musicxmlPath', 'C:/Program Files/MuseScore 4/bin/MuseScore4.exe')
 
-count = 0
+all_X = []
+all_y = []
 for music_file in tqdm(os.listdir(files_path)):
     music_path = os.path.join(files_path, music_file)
     if os.path.getsize(music_path) == 0:
         print(f"{music_path} - пустой")
         continue
-
     midi_data = converter.parse(music_path)
     if len(midi_data.parts) != 2:
         continue
     segments = process_midi(midi_data)
+    for segment in segments:
+        segment_data = extract_notes_from_segment(segment)
+        X, y = prepare_lstm_data(segment_data)
+        all_X.append(X)
+        all_y.append(y)
+
+all_X = np.concatenate(all_X, axis=0)
+all_y = np.concatenate(all_y, axis=0)
+np.save(os.path.join(output_path, 'all_X.npy'), all_X)
+np.save(os.path.join(output_path, 'all_y.npy'), all_y)
